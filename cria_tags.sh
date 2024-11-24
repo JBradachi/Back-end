fb93e6d7ab5dc2c51ce7fa13d6294fb5c54e7a1f
JBradachi <joao.bradachi@gmail.com>
2024-11-24 13:34:14 -0300

    descrição

--- cria_tags.sh
+++ cria_tags.sh
@@ -6,3 +6,4 @@ for brnch in $(git branch -r| grep 'origin/'$1 | sed 's/origin\///'); do
    done
 
 # se tudo ocorrer bem use o comando $ git push origin --tags
+# ele vai enviar todas as tags q não foram enviadas
